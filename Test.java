deb4506b2f9089aa00975d386cb090f5c9d07e39
xy-creature <xy-creature@163.com>
2016-04-16 21:32:23 +0800

    Added files via upload

---
+++ Test.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String line = s.nextLine();
+		String key = s.nextLine();
+		char[] array = line.toCharArray();
+		char[] keyArray = key.toCharArray();
+		int sum = 0;
+		for(int i=0;i<array.length-keyArray.length+1;i++){
+			boolean match = true;
+			for(int j=0;j<keyArray.length;j++){
+				if(array[i+j] != keyArray[j]){
+					match = false;
+					break;
+				}
+	
+			}
+			if(match){
+				sum ++ ;
+			}
+		}
+		System.out.println(sum);
+	}
+}
